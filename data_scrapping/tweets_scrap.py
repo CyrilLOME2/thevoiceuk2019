67d7b88b69df844210ab6466a4fc227361db3ab8
shtheo <theo.ponton@gmail.com>
2019-01-24 11:31:14 +0100

    creation of a new fodler data_scrapping and the descrption of the first file in it

---
+++ data_scrapping/tweets_scrap.py
@@ -0,0 +1,9 @@
+"""
+Created the 24/01/2019
+
+@author: Theo Ponton for #thevoiceuk project
+
+The purpose of the script is to extract tweets with the hastag #thevoiceuk between two dates.
+The result will be store into a csv file named tweets_<dtae1>_<date2>.csv
+
+"""
