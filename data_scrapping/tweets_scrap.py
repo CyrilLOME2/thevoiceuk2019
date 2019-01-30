8e670ca0c487636f20d6271b686ec6beb78ec942
shtheo <theo.ponton@gmail.com>
2019-01-30 17:37:19 +0100

    likes, messages, retweets and scroll down

--- data_scrapping/tweets_scrap.py
+++ data_scrapping/tweets_scrap.py
@@ -19,7 +19,7 @@ from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 # date_1 = str(input('Please choose date_1. Ex : 2019-01-01\n>>>'))
 # date_2 = str(input('Please choose date_2. Ex : 2019-01-03\n>>>'))
 date_1 = '2019-01-04'
-date_2 = '2019-01-07'
+date_2 = '2019-01-11'
 
 # creating selenium driver
 binary = FirefoxBinary(r'C:\Program Files (x86)\Mozilla Firefox\firefox.exe')
@@ -27,15 +27,35 @@ executable_path = r'C:\Users\theop\Documents\Cours\Projet Option\thevoiceuk2019\
 driver = webdriver.Firefox(firefox_binary=binary, executable_path=executable_path)
 
 # reaching the result page
+print('Reaching the page...')
 link = 'https://twitter.com/search?l=&q=%23thevoiceuk%20since%3A' + date_1 + '%20until%3A' + date_2 + '&src=typd'
 driver.get(link)
 time.sleep(5)
+
+# scroll down till the end
+print('Scrolling down...')
+iter = 0
+id_last_tweet = driver.find_elements_by_xpath("//li[@data-item-type='tweet']/div")[-1].get_attribute('data-tweet-id')
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+time.sleep(3)
+id_last_tweet_2 = driver.find_elements_by_xpath("//li[@data-item-type='tweet']/div")[-1].get_attribute('data-tweet-id')
+while id_last_tweet != id_last_tweet_2 and iter < 50:
+    iter += 1
+    id_last_tweet = id_last_tweet_2
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(5)
+    id_last_tweet_2 = driver.find_elements_by_xpath("//li[@data-item-type='tweet']/div")[-1].get_attribute(
+        'data-tweet-id')
+
+print('We scrolled down ' + str(iter) + ' times !')
 
 # where to store de data
 tweets_info = []
 
 # localize each tweet
+print('Finding information...')
 tweets = driver.find_elements_by_xpath("//li[@data-item-type='tweet']/div")
+print('Number of tweets : ' + str(len(tweets)))
 for tweet in tweets:
     tweet_info = {}
     tweet_info['tweet_id'] = tweet.get_attribute('data-tweet-id')
@@ -44,12 +64,19 @@ for tweet in tweets:
     tweet_info['user_name'] = tweet.get_attribute('data-name')
     tweet_info['date_and_time'] = str(tweet.find_element_by_xpath("//div[@data-tweet-id='" + tweet_info['tweet_id'] + "']/div/div/small/a[@class='tweet-timestamp js-permalink js-nav js-tooltip']").get_attribute('title'))
     tweet_info['content'] = tweet.find_element_by_xpath("//div[@data-tweet-id='" + tweet_info['tweet_id'] + "']/div/div[@class='js-tweet-text-container']").text.replace('\n', ' ')
+    tweet_info['nb_replies'] = tweet.find_element_by_xpath("//span[@id='profile-tweet-action-reply-count-aria-" + tweet_info['tweet_id'] + "']").get_attribute('innerHTML')[0]
+    tweet_info['nb_retweets'] = tweet.find_element_by_xpath(
+        "//span[@id='profile-tweet-action-retweet-count-aria-" + tweet_info['tweet_id'] + "']").get_attribute(
+        'innerHTML')[0]
+    tweet_info['nb_jaime'] = tweet.find_element_by_xpath(
+        "//span[@id='profile-tweet-action-favorite-count-aria-" + tweet_info['tweet_id'] + "']").get_attribute(
+        'innerHTML')[0]
     tweets_info.append(tweet_info)
-    print(tweet_info)
 
 # csv output
+print('Writing csv file...')
 with open('tweets_' + date_1 + '_' + date_2 + '.csv', 'w', newline='', encoding='utf-8') as csvfile:
-    fieldnames = ['tweet_id', 'user_id', 'user_pseudo', 'user_name', 'date_and_time', 'content']
+    fieldnames = ['tweet_id', 'user_id', 'user_pseudo', 'user_name', 'date_and_time', 'content', 'nb_replies', 'nb_retweets', 'nb_jaime']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
     for tweet_info in tweets_info:
