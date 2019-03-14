5bf0a5ef1eada23e0528c9727e718b040438dd1a
PapyGuibou <thesuperboss@laposte.net>
2019-03-14 10:49:49 +0100

    Amélioration cleaning

--- Pretraitement.py
+++ Pretraitement.py
@@ -45,45 +45,81 @@ def affiche_tweet(Data,nb = 10):
     for i in range(min(nb,len(Data))):
         print(Data[i][5])
 
-def trim_tweet (tweet, Lcoach = []):
+def check2019 (mot, L):
+    for i in range(len(L)):
+        for j in range(len(L[i])):
+            if L[i][j] in mot:
+                return i
+    return -1
+
+def trim_tweet (tweet, Lcoach = [], Lcandidate = []):
     """ Flag false -> No problem,
         Flag true  -> Destruction du tweet """
     no_tag, two_tags = True, False
-    t = tweet.split()
+    
+    t = tweet.lower().split()
     L_clean = []
+    numCandidate = -1
     for i in range(len(t)):
         if not ("http" in t[i] or "#" in t[i]):
-            if "@" in t[i] :
+            if len(Lcandidate)==0 : # Traitement général
+                if "@" in t[i] :
+                    if t[i] in Lcoach :
+                        L_clean.append("coach")
+                    elif t[i][:9]== "@thevoice" :
+                        L_clean.append("thevoice")
+                    else :
+                        L_clean.append("candidate")
+                else :
+                    L_clean.append(t[i])
+                    
+            else :  # Traitement des tweets 2019
                 if t[i] in Lcoach :
                     L_clean.append("coach")
-                elif t[i][:9]== "@thevoice" :
+                elif t[i] == "@thevoice" :
                     L_clean.append("thevoice")
                 else :
-                    L_clean.append("candidate")
-                    if not(no_tag) :
+                    num = check2019(t[i], Lcandidate)
+                    if num == -1 :
+                        if '@' in t[i] :
+                            L_clean.append("person")
+                        else :
+                            L_clean.append(t[i])
+                    elif (numCandidate != -1) and (num != numCandidate):
                         two_tags = True
-                    no_tag = False
-            else :
-                L_clean.append(t[i])
+                    else :
+                        no_tag = False
+                        numCandidate = num
+                        L_clean.append("candidate")
     
     tweet = ''
     for l in L_clean:
         tweet = tweet + l + " "
     tweet_emojis = ''.join([tweet[i] for i in range(len(tweet)) if not (tweet[i] in ("0.123456789abcdefghijklmnopqrstuvwxyz ,!:?;']ABCDEFGHIJKLMNOPQRSTUVWXYZ()<>-_&%£’áóé~èç$€"+'"'))])
     emoji.write(tweet_emojis)
-    tweet = ''.join(re.findall(r'[0.123456789abcdefghijklmnopqrstuvwxyz ,!:?;]', tweet.lower()))
-    return tweet, (len(tweet) < 15 or len(re.findall(r'[éèàùã]', tweet))>0 or no_tag or two_tags)
+    tweet = ''.join(re.findall(r'[0.123456789abcdefghijklmnopqrstuvwxyz !:?;]', tweet))
+    tweetValide = (len(tweet) < 15 or len(re.findall(r'[éèàùã]', tweet))>0 or no_tag or two_tags)
+    return tweet, tweetValide, numCandidate
 
 def trim_data (Data,name_new_file = file[:-4]+"_clean.csv", Lcoach = []):
     """Epure les tweets, donne le score d'importance et écrit dans le fichier"""
     # tweet_id,user_id,user_pseudo,user_name,date_and_time,content,nb_replies,nb_retweets,nb_jaime
+    if '2019' in name_new_file :
+        fcandidate = open('Cadidate2019.txt', 'r', encoding = 'utf-8')
+        Lcandidate = fcandidate.readlines()
+        fcandidate.close()
+        Lcandidate = [l.lower() for l in Lcandidate]
+        Lcandidate = [Lcandidate[i][:-1].split(' ') for i in range(len(Lcandidate))]
+    else :
+        Lcandidate = []
     f = open("Cleaned/"+name_new_file,"w", encoding="utf-8")
-    f.write("idTweet, clean_tweet, scoreImportance, idCandidat\n")
+    f.write("idTweet, clean_tweet, scoreImportance, idCandidat, smileyHappy, smileySad\n")
     for i in range(len(Data)):
-        clean_tweet, b = trim_tweet(Data[i][5], Lcoach)
-        if not b :
-            score_impor = int(log(2+int(Data[i][6])+int(Data[i][7])+int(Data[i][8]),10))
-            f.write(Data[i][0]+','+clean_tweet+','+str(score_impor)+'\n')
+        clean_tweet, b, numC = trim_tweet(Data[i][5], Lcoach, Lcandidate)
+        if (not b) and (len(Lcandidate)==0 or numC!=-1) :
+            score_impor = int(log(10+int(Data[i][6])+int(Data[i][7])+int(Data[i][8]),10))
+            #csv clean -> id         tweet            scoreimp          candidat    emojiHappy  sadEmoji
+            f.write(Data[i][0]+','+clean_tweet+','+str(score_impor)+','+str(numC)+','+str(0)+','+str(0)+'\n')
     f.close()
 
 def clean_file (path, filename):
