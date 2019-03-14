cba3dcbb8403dce3f826f8788051d0269283bed6
shtheo <theo.ponton@gmail.com>
2019-03-14 11:25:12 +0100

    till March 11th

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+Created the 14/03/2019
+
+@author: All team members from for #thevoiceuk project
+
+This scipt will import all the functions that have been done during to project to build the whiole data pipeline
+from data srapping to the final ranking.
+
+"""
+from tweets_scrap import *
+from algo_final import *
+# from notation import *
+from Pretraitement import *
+
+print('coucou')
