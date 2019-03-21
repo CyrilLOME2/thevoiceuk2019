9956d905244ca7a4c9fa2c894c83b29bfcfb4651
shtheo <theo.ponton@gmail.com>
2019-03-21 12:48:50 +0100

    synchro

--- algo_final.py
+++ algo_final.py
@@ -13,15 +13,15 @@ def write_csv_calculated_marks(path_input, path_output):
         for row in reader :
             csv_notes = {}
             csv_notes['id_tweet'] = row['id_tweet']
-            csv_notes['id_candidat'] = row['id_candidat']
-            csv_notes['sum_rt_com_like'] = row['sum_rt_com_like']
+            csv_notes['id_candidat'] = row['id_candidate']
+            csv_notes['sum_rt_com_like'] = row['score_importance']
             sum_mark = 0
             for i in range(1,6):
-                sum_mark += int(row['note_proche_voisin_' + str(i)])
-            if row['smiley_happy'] == '0' and row['smiley_unhappy'] == '0':
+                sum_mark += int(row['notation_' + str(i)])
+            if row['nb_smiley_happy'] == '0' and row['nb_smiley_unhappy'] == '0':
                 csv_notes['note'] = sum_mark / 5
             else :
-                note_smiley = calculate_smiley_mark(int(row['smiley_happy']), int(row['smiley_unhappy']))
+                note_smiley = calculate_smiley_mark(int(row['nb_smiley_happy']), int(row['nb_smiley_unhappy']))
                 csv_notes['note'] = (sum_mark + note_smiley) / 6
             csv_notes_list.append(csv_notes)
     with open(path_output, 'w', newline='') as csvfile:
@@ -54,7 +54,7 @@ def write_csv_calculating_ranking(path_input, path_output):
     with open(path_input) as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            score = log(int(row["sum_rt_com_like"])) * (float(row["note"]) - 3)
+            score = log(int(row["sum_rt_com_like"]) + 2) * (float(row["note"]) - 2.5)
             if row["id_candidat"] not in score_candidate:
                 score_candidate[row["id_candidat"]] = score
             else :
@@ -84,10 +84,10 @@ def write_csv_calculating_ranking(path_input, path_output):
 
 if __name__ == '__main__':
 
-    path_input = "template/output_W2V.csv"
-    path_output = "template/output_marks_for_each_tweet.csv"
+    path_input = "template/5nn-results-tweets_2019-01-04_2019-01-30_clean.csv"
+    path_output = "template/mark-5nn-results-tweets_2019-01-04_2019-01-30_clean.csv"
     write_csv_calculated_marks(path_input, path_output)
 
-    path_input = "template/output_marks_for_each_tweet.csv"
-    path_output = "template/output_final.csv"
+    path_input = "template/mark-5nn-results-tweets_2019-01-04_2019-01-30_clean.csv"
+    path_output = "template/ranking-mark-5nn-results-tweets_2019-01-04_2019-01-30_clean.csv"
     write_csv_calculating_ranking(path_input, path_output)
